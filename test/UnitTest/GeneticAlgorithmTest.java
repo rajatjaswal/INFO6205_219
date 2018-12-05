5b77986e6d4269e7630063f54510354058e8b9c8
srishti <saboo.s@husky.neu.edu>
2018-12-04 21:26:03 -0500

    Test Cases

---
+++ test/UnitTest/GeneticAlgorithmTest.java
@@ -0,0 +1,21 @@
+package UnitTest;
+
+import org.junit.Test;
+
+/**
+ * Test method of GeneticAlgorithm.
+ */
+public class GeneticAlgorithmTest {
+	@Test
+    public void testInit() throws Exception {
+		
+	}
+	@Test
+    public void testMutation() throws Exception {
+		
+	}
+	@Test
+    public void testEvolve() throws Exception {
+		
+	}
+}
