09212f2dd466946bb63e37c8ccd67764c51fb711
srishti <saboo.s@husky.neu.edu>
2018-12-05 20:31:14 -0500

    Test Cases

--- test/UnitTest/FitnessTest.java
+++ test/UnitTest/FitnessTest.java
@@ -1,15 +1,17 @@
 package UnitTest;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 
-
+import org.junit.Test;
 
 import GA.FitnessFunction;
 import GA.Genotype;
 import GA.IndividualImage;
 import ImageProcess.ProcessImage;
+import ImageProcess.RGB;
 
 /**
  * Test method of GeneticAlgorithm.
@@ -19,16 +21,18 @@ public class FitnessTest {
 	@Test
     public void testFitness() throws Exception {
 		// Read two files
-		BufferedImage oImage = ImageIO.read(new File('testImages/human.png'););
-		File file1 = new File('testImages/1.png');
+		System.out.println("asdsadasd");
+		File filex = new File("testImages/human.png");
+		BufferedImage oImage = ImageIO.read(filex);
+		File file1 = new File("testImages/1.png");
 		BufferedImage image1 = ImageIO.read(file1);
-		File file2 = new File('testImages/51.png');
+		File file2 = new File("testImages/51.png");
 		BufferedImage image2 = ImageIO.read(file2);
 		RGB[][] originalRGB=ProcessImage.readValuesAsPixels(oImage);
 		
-		fitness1 = FitnessFunction.evaluateFitnessMain(image1, originalRGB);
-		fitness2 = FitnessFunction.evaluateFitnessMain(image2, originalRGB);
+		long fitness1 = FitnessFunction.evaluateFitnessMain(image1, originalRGB);
+		long fitness2 = FitnessFunction.evaluateFitnessMain(image2, originalRGB);
 		System.out.println(fitness1 + " "+fitness2);
-		assert(fitness1, fitness2);
+//		assert(fitness1, fitness2);
 	}
 }
--- test/UnitTest/MutationTest.java
+++ test/UnitTest/MutationTest.java
@@ -1,5 +1,19 @@
 package UnitTest;
 
-public class MutationTest {
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+public class MutationTest {
+	@Test
+	public void testMutation() throws Exception{
+//		File file1 = new File("testImages/1.png");
+//		BufferedImage image1 = ImageIO.read(file1);
+//		File file2 = new File("testImages/2.png");
+//		BufferedImage image2 = ImageIO.read(file2);
+//		RGB[][] image1RGB 
+	}
 }
